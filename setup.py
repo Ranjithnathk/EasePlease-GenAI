f700febd96c5704f4d063e68fb5a08dfa56e8afa
Ranjithnathk <ranjithnathk29@gmail.com>
2025-05-25 18:30:34 -0400

    project folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'EasePlease - GenAI Project',
+    version= '0.0.0',
+    author= 'Ranjithnath Karunanidhi',
+    author_email= 'ranjithnathk29@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
